a0eaa23005f1aed23a7986963fb91b2d2ed6ef79
Ivan Kuzmenko <6745157+rndtrash@users.noreply.github.com>
2021-03-10 18:38:21 +0300

    Config system, experiments with middleware

--- src/server.js
+++ src/server.js
@@ -4,12 +4,15 @@ const service = require("restana")();
 const v1 = require("./v1");
 
 module.exports.run = (params) => {
+	// Middleware
 	service.use(require("response-time")());
+
+	// API
 	service.use("/v1", v1);
 
 	http
 		.createServer(service)
-		.listen(params.port || 8579, params.host || "0.0.0.0", function () {
+		.listen(params.server.port || 8579, params.server.host || "0.0.0.0", function () {
 			console.log("running");
 		});
 };
